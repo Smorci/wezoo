5f5f1d58d8b8c0f3e857b77d100ad41d88e15b5b
Smorci <szekelyschmarton@gmail.com>
2024-04-23 22:58:58 +0100

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+#![deny(warnings)]
+
+use tokio;
+use pretty_env_logger;
+
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+
+    let frontend = warp::fs::dir("out");
+
+    warp::serve(frontend)
+        .run(([127, 0, 0, 1], 3030))
+        .await;
+}
+
+#[cfg(test)]
+mod tests {
+
+    use http::{Request};
+
+    #[test]
+    fn frontend_http_get() {
+        let request = Request::builder()
+            .method("GET")
+            .uri("http://localhost:3030/")
+            .unwrap();
+    }
 }
